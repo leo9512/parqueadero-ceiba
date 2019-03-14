29b6d630dab52140f05e795d1545b8f00838089b
unknown <leodaza1995@gmail.com>
2019-03-13 20:03:30 -0500

    first commit

---
+++ src/main/java/co/com/ceiba/estacionamiento/neyderdaza/CeibaEstacionamiento/neyder/daza/Application.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.estacionamiento.neyderdaza.CeibaEstacionamiento.neyder.daza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ceiba/estacionamiento/neyderdaza/CeibaEstacionamiento/neyder/daza/ApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.ceiba.estacionamiento.neyderdaza.CeibaEstacionamiento.neyder.daza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
